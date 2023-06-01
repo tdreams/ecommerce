9b789b8d9e7f965402cf5227c676800a7205463f
tdreams <fabriels@gmail.com>
2023-06-01 08:51:44 -0700

    category

---
+++ src/components/category.jsx
@@ -0,0 +1,27 @@
+const category = [
+  {
+    id: "cacZkNf2kwmdBcqd0",
+    name: "Jordan",
+    image:
+      "https://media.finishline.com/i/finishline/DM9652_102_P1?$default$&w=671&&h=671&bg=rgb(237,237,237)",
+  },
+  {
+    id: "cacotY5Nh00DQFdkm",
+    name: "NIKE",
+    image:
+      "https://media.finishline.com/i/finishline/DJ5423_100_P1?$default$&w=671&&h=671&bg=rgb(237,237,237)",
+  },
+  {
+    id: "cac1Ntk7siEEW9ha1",
+    name: "CONVERSE",
+    image:
+      "https://media.finishline.com/i/finishline/M3310_BLK_P1?$default$&w=671&&h=671&bg=rgb(237,237,237)",
+  },
+  {
+    id: "cacoW8ecgjtKx2Sj2",
+    name: "ADIDAS",
+    image:
+      "https://media.finishline.com/i/finishline/GW4661_107_P1?$default$&w=671&&h=671&bg=rgb(237,237,237)",
+  },
+];
+export default category;
--- src/components/home/Feature.jsx
+++ src/components/home/Feature.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import category from "../category";
 
 const Feature = () => {
   return (
@@ -24,6 +25,24 @@ const Feature = () => {
           <a>
             <NavLink>Girls' Sgoes</NavLink>
           </a>
+        </div>
+      </div>
+      <div>
+        <h2>SHOP BY CATEGORY</h2>
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {category.map((brand) => {
+            return (
+              <div key={brand.id} className="group relative ">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-[#EDEDED] lg:aspect-none  lg:h-80">
+                  <img
+                    src={brand.image}
+                    alt={brand.image}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full  "
+                  />
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
